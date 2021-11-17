ce27466d04f3eeca3ea0117c08d4bc5d0fd66c90
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-11-17 13:45:48 +0530

    Update insta_cmds.py

--- plugins/insta_cmds.py
+++ plugins/insta_cmds.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2021 subinps
+#Copyright (c) 2021 Jimi
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -40,16 +40,16 @@ insta = Config.L
 buttons=InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-            InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+            InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/Jimi_Bots'),
+            InlineKeyboardButton("🎵😎💓", url="")
         ],
         [
-            InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-            InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+        
+            InlineKeyboardButton("Keep Smiling 😀 ", url="")
         ],
         [
             InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin"),
-            InlineKeyboardButton("⚙️Update Channel", url="https://t.me/subin_works")
+            InlineKeyboardButton("⚙️Update Channel", url="https://t.me/Jimi_Bots")
         ]
 					
     ]
